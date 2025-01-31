0c8d9880867424365714b86cf4ccf0794a7a6847
TangJingfan <tangjingfan@gmail.com>
2025-01-31 19:52:25 +0800

    refactor: redesign motor control interface

--- include/uav_motor.h
+++ include/uav_motor.h
@@ -1,6 +1,16 @@
 #ifndef UAV_MOTOR_H
 #define UAV_MOTOR_H
 
-void set_motor_speed();
+#include "state.h"
+
+extern int motor_nums;
+
+extern int motor[];
+
+extern int speed[];
+
+extern int throttle[];
+
+void set_motor(State current_state);
 
 #endif
--- src/uav_motor.cpp
+++ src/uav_motor.cpp
@@ -1,48 +1,36 @@
-#include "config.h"
+#include "uav_motor.h"
 #include "port.h"
+#include "state.h"
 #include <Arduino.h>
 
+// # of motors
+int motor_nums = 4;
+
+// array of motor
 int motor[] = {PWM1, PWM2, PWM3, PWM4};
 
-int speed[] = {-1, -1, -1, -1};
+// array of speed
+// int of 0 - 255
+int speed[] = {0, 0, 0, 0};
 
-int current_yaw_pitch_roll[] = {0, 0, 0};
-int target_yaw_pitch_roll[] = {0, 0, 0};
-int error_yaw_pitch_roll[] = {0, 0, 0};
+// array of throttle
+// definition: minimum voltage for motor to make uav fly
+int throttle[] = {60, 60, 60, 60};
 
-int throttle[] = {100, 100, 100, 100};
-int min_motor_speed = 0;
-int max_motor_speed = 255;
-
-void motor_set_speed() {
-  for (int i = 0; i < motor_num; i++) {
-    analogWrite(motor[i], speed[i]);
+/**
+ * @brief set motor speed according to speed[]
+ * @param current_state
+ * * STOP when current_state == STATE_STOP
+ * * RUN when current_state == STATE_RUN
+ */
+void set_motor(State current_state) {
+  if (current_state == STATE_RUN) {
+    for (int i = 0; i < motor_nums; i++) {
+      analogWrite(motor[i], speed[i]);
+    }
+  } else {
+    for (int j = 0; j < motor_nums; j++) {
+      analogWrite(motor[j], 0);
+    }
   }
-}
-
-void motor_set_speed(int speed_fwd, int speed_left, int speed_bwd,
-                     int speed_right) {
-  int speed_temp[] = {speed_fwd, speed_left, speed_bwd, speed_right};
-  for (int i = 0; i < motor_num; i++) {
-    analogWrite(motor[i], speed_temp[i]);
-  }
-}
-
-void update_motor_speeds(float roll_output, float pitch_output,
-                         float yaw_output) {
-  // calculate speed of each motor
-  int M1 =
-      static_cast<int>(throttle[0] - roll_output - pitch_output + yaw_output);
-  int M2 =
-      static_cast<int>(throttle[1] + roll_output - pitch_output - yaw_output);
-  int M3 =
-      static_cast<int>(throttle[2] - roll_output + pitch_output - yaw_output);
-  int M4 =
-      static_cast<int>(throttle[3] + roll_output + pitch_output + yaw_output);
-
-  // constrain voltage
-  speed[0] = constrain(M1, min_motor_speed, max_motor_speed);
-  speed[1] = constrain(M2, min_motor_speed, max_motor_speed);
-  speed[2] = constrain(M3, min_motor_speed, max_motor_speed);
-  speed[3] = constrain(M4, min_motor_speed, max_motor_speed);
 }
