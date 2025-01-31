9eed61c8ac18b881151f7ee519350541fbfec658
TangJingfan <tangjingfan@gmail.com>
2025-01-31 16:50:59 +0800

    feat: calculate euler angle accurately

--- include/imu.h
+++ include/imu.h
@@ -3,20 +3,28 @@
 
 #include <MPU6500_WE.h>
 
+#define imu_x_axis 0
+#define imu_y_axis 1
+#define imu_z_axis 2
+
+#define uav_roll 0
+#define uav_pitch 1
+#define uav_yaw 2
+
+/**
+ * we get roll angle from x_axis
+ * we get pitch angle from y_axis
+ */
+extern float attitude[];
+
 /**
  * @brief basic setup of imu MPU6500
  */
 void imu_setup();
 
 /**
- * @brief get raw message from sensor
- * TODO: read message
- */
-void read_sensor();
-
-/**
- * @brief calcualte euler angle by using raw data
- * TODO: calculation
+ * @brief calculate Euler angle
+ * ! the value of yaw is not reliable
  */
 void calculate_euler_angle();
 
--- src/imu.cpp
+++ src/imu.cpp
@@ -1,29 +1,33 @@
 #include "imu.h"
 #include "port.h"
-#include "uav_motor.h"
 #include <Arduino.h>
 #include <MPU6500_WE.h>
 #include <Wire.h>
 #include <cmath>
 
+// MPU6500 address
 #define MPU6500_ADDR 0x68
 
+// an object of MPU6500
 MPU6500_WE MPU6500_on_drone = MPU6500_WE(MPU6500_ADDR);
 
+// attitude array of uav
+float attitude[] = {0, 0, 0};
+
 void imu_setup() {
-  // set I2C pin
+  // 1. set I2C pin
   Wire.setSCL(I2C_SCL);
   Wire.setSDA(I2C_SDA);
   Wire.begin();
 
-  // init MPU6500
+  // 2. init MPU6500
   if (!MPU6500_on_drone.init()) {
     Serial2.println("MPU6500 does not respond...");
   } else {
     Serial2.println("MPU6500 is connected!");
   }
 
-  // calibrating imu
+  // 3. calibrating imu
   Serial2.println(
       "Position you MPU6500 flat and don't move it - calibrating...");
   // wait for sensor to be stable
@@ -31,14 +35,25 @@ void imu_setup() {
   MPU6500_on_drone.autoOffsets();
   Serial2.println("Done!");
 
-  // setup gyroscope
+  // 4. setup gyroscope
   MPU6500_on_drone.enableGyrDLPF();
   MPU6500_on_drone.setGyrDLPF(MPU6500_DLPF_6);
-  MPU6500_on_drone.setSampleRateDivider(5);
+  // sampling frequency: 50 Hz
+  MPU6500_on_drone.setSampleRateDivider(19);
   MPU6500_on_drone.setGyrRange(MPU6500_GYRO_RANGE_250);
 
-  // setup accelerometer
+  // 5. setup accelerometer
   MPU6500_on_drone.setAccRange(MPU6500_ACC_RANGE_2G);
   MPU6500_on_drone.enableAccDLPF(true);
   MPU6500_on_drone.setAccDLPF(MPU6500_DLPF_6);
+}
+
+void calculate_euler_angle() {
+  // 1. use getAngles()
+  xyzFloat angle = MPU6500_on_drone.getAngles();
+
+  // 2. assign values
+  attitude[imu_x_axis] = angle.x;
+  attitude[imu_y_axis] = angle.y;
+  attitude[imu_z_axis] = angle.z;
 }
